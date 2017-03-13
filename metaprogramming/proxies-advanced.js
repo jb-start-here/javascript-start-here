80f1b0c8808e027161a6628223ccaf835e0c6ae3
joelbandi <joelvivekbandi@gmail.com>
2017-03-13 15:50:35 -0700

    finlize

--- metaprogramming/proxies-advanced.js
+++ metaprogramming/proxies-advanced.js
@@ -1,51 +1,70 @@
-// // Let's explore more traps in the handler object
+// Let's explore more traps in the handler object
 
 
-// const target = {};
+const target = {};
 
-// const handler = {
+const handler = {
 
-//   // This is regular ol' get trap
-//   get: (t,k) => {
-//     console.info(`get trap invoked -> getting { ${k} : ${t[k]} }`);
-//     return t[k];
-//   },
-
-//   // The set trap is invoked when we are trying to set a property of a target through its proxy
-//   set: (t,k,v) => {
-//     console.info(`set trap invoked -> setting { ${k} : ${v} }`);
-//     // We need to return true from a set trap.
-//     // if we return false it returns a TypeError in strict mode and fail silently otherwise
-//     return true;
-//   }
-
-// }
-
-// const proxy = new Proxy(target,handler);
-
-// proxy.time = '10:30am';
-// console.log(proxy.time);
-
-
-
-
-
-
-
-var handler = {
-  get (target, key) {
-    console.log('get');
-    return target[key]
+  // This is regular ol' get trap
+  get: (t,k) => {
+    console.info(`get trap invoked -> getting { ${k} : ${t[k]} }`);
+    return t[k];
   },
-  set (target, key, value) {
-    console.log('set');
-    target[key] = value; // what the fuck is going on???
-    return true
+
+  // The set trap is invoked when we are trying to set a property of a target through its proxy
+  set: (t,k,v) => {
+    console.info(`set trap invoked -> setting { ${k} : ${v} }`);
+    // We need to return true from a set trap.
+    // if we return false it returns a TypeError in strict mode and fail silently otherwise
+    t[k] = v;
+    return true;
+  }
+
+}
+
+var proxy = new Proxy(target,handler);
+
+proxy.time = '10:30am';
+console.log(proxy.time);
+
+
+
+
+
+
+
+// Use cases
+// Since handlers can intercept access to objects libraries can prevent access by end users to forbidden properties
+// For example..Let's say all private properties are prefixed with _
+
+const secureHandler = {
+  get: (t,k) => {
+    securityCheck(k,'get');
+    return t[k];
+  },
+  set: (t,k,v) => {
+    securityCheck(k,'set');
+    t[k] = v;
+    return true;
   }
 }
 
-var target = {}
-var proxy = new Proxy(target, handler)
+const securityCheck = (key,action) => {
+  if(key[0] === '_'){
+    throw new Error(`Cannot ${action} private property ${key} of this target`);
+  } 
+}
 
-proxy.a = 'a'
-console.info(proxy.a);
+const secureTarget= {}
+
+proxy = new Proxy(secureTarget,secureHandler);
+
+proxy.a = 'a';
+console.log(proxy.a);
+// <- a
+
+
+proxy._secureProp = 'not_secure';
+// <- throws error here
+console.log(proxy._secureProp);
+// <- throws error here as well
